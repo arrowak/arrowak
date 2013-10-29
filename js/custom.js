14cc675d9bea0cf661c3b782d21b487e21f932e1
Arun Kolhapur <arunko350@gmail.com>
2013-10-29 23:20:44 +0530

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.nav-bar ul li').click(function(){
+		var name = $(this).data('name');
+		var elem = $('.'+name);
+		// console.log($('.'+name).position().top);
+		$('html, body').animate({
+			scrollTop: elem.offset().top + 200
+		}, 500);
+
+	});
+});
